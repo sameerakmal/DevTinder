7771c6621c63c9ba7ab1e673a1323179a7a4fe11
Sameer <sameer2akhmal@gmail.com>
2026-02-03 23:35:49 +0530

    updated db password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -15,6 +15,7 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
     }
 });
 
+
 profileRouter.patch("/profile/edit", userAuth, async(req, res) => {
     try{
         if(!validEditProfileData){
